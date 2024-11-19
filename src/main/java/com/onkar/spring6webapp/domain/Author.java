4ac9f8419e420d476e76bd5fcca9961a958ca8c3
Onkar Dhengale <onkar.dhengale@sas.com>
2024-11-19 16:38:24 -0500

    adding equals and hashcode to determine if two objects are equal or not, also toString method

--- src/main/java/com/onkar/spring6webapp/domain/Author.java
+++ src/main/java/com/onkar/spring6webapp/domain/Author.java
@@ -3,6 +3,7 @@ package com.onkar.spring6webapp.domain;
 import jakarta.persistence.*;
 
 import javax.annotation.processing.Generated;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -13,6 +14,8 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+
+
 
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
@@ -49,5 +52,33 @@ public class Author {
         this.firstName = firstName;
     }
 
+//toString method allows to create a string which can be printed easily
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    //Equals and hashcode methods allows hibernate to distinguish between two obejcts ie.
+    //in this case we have made id as comparing part if two objects have same id then
+    //they will be considered as same.
+    //used generate from intellij to create this .
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+        return Objects.equals(getId(), author.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
 
 }
--- src/main/java/com/onkar/spring6webapp/domain/Book.java
+++ src/main/java/com/onkar/spring6webapp/domain/Book.java
@@ -3,6 +3,7 @@ package com.onkar.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -14,6 +15,8 @@ public class Book {
     private String title;
     private String isbn;
 //getters and setters for book
+
+
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -50,5 +53,29 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(getId(), book.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 }
